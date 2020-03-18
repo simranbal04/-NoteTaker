62cc2c0cb4ad293f384af6ea700f6dd39c214b77
Simranbal04 <simranbal01@gmail.com>
2020-03-17 21:04:58 -0400

    Initial Commit

---
+++ app/src/main/java/com/simrankaurbal/editor/Note.java
@@ -0,0 +1,15 @@
+package com.simrankaurbal.editor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Note extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstatnceState)
+    {
+        super.onCreate(savedInstatnceState);
+        setContentView(R.layout.edit1);
+    }
+}
