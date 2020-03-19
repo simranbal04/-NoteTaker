07f129f14c464723a2ef249aa60318a8ffd89e92
Simranbal04 <simranbal01@gmail.com>
2020-03-18 23:54:44 -0400

    Refactor

--- app/src/main/java/com/simrankaurbal/editor/MainActivity.java
+++ app/src/main/java/com/simrankaurbal/editor/MainActivity.java
@@ -2,13 +2,52 @@ package com.simrankaurbal.editor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        notes.add("Testing");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(getApplicationContext(),Note.class);
+                i.putExtra("noteid",position);
+                startActivity(i);
+            }
+        });
+
+
+        // feature when user taps on the item in listview it goes to new activity
+
+//        listView.setOnClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//                Intent intent = new Intent(getApplicationContext(),Note.class);
+//                intent.putExtra("noteid",position);
+//                startActivity(intent);
+//
+//
+//            }
+//        });
+
     }
 }
